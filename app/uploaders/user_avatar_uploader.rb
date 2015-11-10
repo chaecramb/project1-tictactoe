3faec68644e80f08d30973698bb3dc215117aedc
Chae Cramb <chaecramb@gmail.com>
2015-11-10 16:52:14 +0000

    add user index page

--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -23,13 +23,13 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
   version :medium do
     process :resize_to_fit => [400, 400]
   end
+
+  def default_url
+    ActionController::Base.helpers.asset_path("default_avatars/#{(1..7).to_a.sample}.png")
+  end
+
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
