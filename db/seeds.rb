f86b8baa5319c50cc2c23dc4a8d744d9fd068d66
Chae Cramb <chaecramb@gmail.com>
2015-11-11 18:43:44 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,24 +15,28 @@
 # g.moves << Move.create(player_id: 1, game_id: 1, square: 1) 
 
 guest = User.create(name: 'Guest', email:'guest@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Play against a guest on your own account")
-bender = User.create(name: 'Bender', email:'dumbai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Bender's drunk and seems to be making moves at random")
-kryten = User.create(name: 'Kryten', email:'kindai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Kryten just wants to keep you happy. He tries to let you win all the time")
-hal = User.create(name: 'HAL 9000', email:'strongai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "I'm sorry Dave. I'm afraid i can't do that.")
-r2d2 = User.create(name: 'R2-D2', email:'medai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "R2D2 likes a good game, so he'll give you a chance.")
-marvin = User.create(name: 'Marvin', email:'neuroticai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Marvin's paranoia make him an unpredictable opponent!")
-spiderman = User.create(name: 'Spiderman', email:'spiderman@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player')
+bender = User.create(name: 'Bender (AI)', email:'dumbai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Bender's drunk and seems to be making moves at random")
+kryten = User.create(name: 'Kryten (AI)', email:'kindai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Kryten wants to keep you happy, he'll try to let you win")
+hal = User.create(name: 'HAL 9000 (AI)', email:'strongai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "I'm sorry Dave. I'm afraid i can't do that.     ")
+r2d2 = User.create(name: 'R2-D2 (AI)', email:'medai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "R2D2 likes a fair game, so he'll give you a chance.")
+marvin = User.create(name: 'Marvin (AI)', email:'neuroticai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "His paranoia makes him an unpredictable opponent!")
+spiderman = User.create(name: 'Spider-Man', email:'spiderman@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Your friendly neighbourhood Spider-Man.")
+batman = User.create(name: 'Batman', email:'batman@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Bats frighten me. It’s time my enemies share my dread.")
 
-guest.recipe_image = Rails.root.join("db/images/guest.jpg").open
+guest.user_avatar = Rails.root.join("db/images/guest.jpg").open
 guest.save
-bender.recipe_image = Rails.root.join("db/images/bender.png").open
+bender.user_avatar = Rails.root.join("db/images/bender.jpeg").open
 bender.save
-kryten.recipe_image = Rails.root.join("db/images/kryten.gif").open
+kryten.user_avatar = Rails.root.join("db/images/kryten.gif").open
 kryten.save
-hal.recipe_image = Rails.root.join("db/images/hal.png").open
+hal.user_avatar = Rails.root.join("db/images/hal.png").open
 hal.save
-r2d2.recipe_image = Rails.root.join("db/images/r2d2.jpg").open
+r2d2.user_avatar = Rails.root.join("db/images/r2d2.jpg").open
 r2d2.save
-marvin.recipe_image = Rails.root.join("db/images/marvin.jpg").open
+marvin.user_avatar = Rails.root.join("db/images/marvin.jpg").open
 marvin.save
-spiderman.recipe_image = Rails.root.join("db/images/bender2.jpeg").open
+spiderman.user_avatar = Rails.root.join("db/images/spiderman.jpg").open
 spiderman.save
+batman.user_avatar = Rails.root.join("db/images/batman.jpeg").open
+batman.save
+
