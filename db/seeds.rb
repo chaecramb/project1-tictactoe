860d67cff7b3b3d1efcfa67536bfef601377a0e8
Chae Cramb <chaecramb@gmail.com>
2015-11-11 18:19:29 +0000

    seed avatars

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,25 @@
 # g.moves << Move.create(player_id: 2, game_id: 1, square: 6) 
 # g.moves << Move.create(player_id: 1, game_id: 1, square: 1) 
 
-User.create(name: 'Guest', email:'guest@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Play against a guest on your own account")
-User.create(name: 'Bender', email:'dumbai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Bender's drunk and seems to be making moves at random")
-User.create(name: 'Kryten', email:'kindai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Kryten just wants to keep you happy. He tries to let you win all the time")
-User.create(name: 'HAL 9000', email:'strongai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "I'm sorry Dave. I'm afraid i can't do that.")
-User.create(name: 'R2-D2', email:'medai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "R2D2 likes a good game, so he'll give you a chance.")
-User.create(name: 'Marvin', email:'neuroticai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Marvin's paranoia make him an unpredictable opponent!")
-User.create(name: 'Chae', email:'chae@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player')
+guest = User.create(name: 'Guest', email:'guest@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Play against a guest on your own account")
+bender = User.create(name: 'Bender', email:'dumbai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Bender's drunk and seems to be making moves at random")
+kryten = User.create(name: 'Kryten', email:'kindai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Kryten just wants to keep you happy. He tries to let you win all the time")
+hal = User.create(name: 'HAL 9000', email:'strongai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "I'm sorry Dave. I'm afraid i can't do that.")
+r2d2 = User.create(name: 'R2-D2', email:'medai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "R2D2 likes a good game, so he'll give you a chance.")
+marvin = User.create(name: 'Marvin', email:'neuroticai@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player', desc: "Marvin's paranoia make him an unpredictable opponent!")
+spiderman = User.create(name: 'Spiderman', email:'spiderman@email.example', password: 'password', created_at: Time.now, updated_at: Time.now, role: 'player')
+
+guest.recipe_image = Rails.root.join("db/images/guest.jpg").open
+guest.save
+bender.recipe_image = Rails.root.join("db/images/bender.png").open
+bender.save
+kryten.recipe_image = Rails.root.join("db/images/kryten.gif").open
+kryten.save
+hal.recipe_image = Rails.root.join("db/images/hal.png").open
+hal.save
+r2d2.recipe_image = Rails.root.join("db/images/r2d2.jpg").open
+r2d2.save
+marvin.recipe_image = Rails.root.join("db/images/marvin.jpg").open
+marvin.save
+spiderman.recipe_image = Rails.root.join("db/images/bender2.jpeg").open
+spiderman.save
