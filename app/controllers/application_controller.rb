9f494a33da8d6fb82a9b00b84de9996757af309e
Chae Cramb <chaecramb@gmail.com>
2015-11-11 10:52:38 +0000

    dropshadows and users index styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: "You can't access this page"
+  end
+
   protected
 
    def configure_devise_permitted_parameters
